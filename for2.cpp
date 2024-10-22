6e568029ceeb9fa7b7746adc62bec3c24848a4ad
overlord-repo <lapukaegor@gmail.com>
2024-10-22 11:09:07 +0200

    Create for2.cpp

---
+++ for2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //słowa kluczowe: for, while, do while
+    for(int i=20;i>=0;i--) {
+    cout << "Licznik i wynosi: " << i << endl;
+    }
+    for(int i=20;i>=-432;i--) {
+    cout << "Licznik i wynosi: " << i << endl; 
+    }
+    return 0;
+}
