d30fa5d842d6e13170fd79b1f75935ca641e816a
overlord-repo <lapukaegor@gmail.com>
2024-10-22 11:24:04 +0200

    Create whilepulapka.cpp

---
+++ whilepulapka.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+double a = 1;
+double b = 2;
+
+int main()
+{
+    //słowa kluczowe: for, while, do while
+    int i=0;
+    while (i<5) { //pętla nieskonczona
+        cout << (a++)*b++/a*100*b << "\n";
+        i--;
+    }
+    while (i<5) { //juz zadeklarowaliśmy i, więc nie będzie działało
+        cout << (a++)*b++/a*100*b << "\n";
+        i--;
+    }
+    return 0;
+}
