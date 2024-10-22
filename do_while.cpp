bce3d7905a7023ad0914b933525fb23a59e43f46
overlord-repo <lapukaegor@gmail.com>
2024-10-22 11:26:42 +0200

    Create do_while.cpp

---
+++ do_while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //słowa kluczowe: for, while, do while
+    int i=10;
+    do {
+        cout <<"mam wypisywać wartość licznika do póki jest on mniejszy od 5; ";
+    } while (i<5);
+    return 0;
+}
